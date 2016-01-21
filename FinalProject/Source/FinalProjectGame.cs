1c548fd974ecd805b770be44b30f008a19f66467
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-01-21 23:17:48 +0000

    Tidy up game class

---
+++ FinalProject/Source/FinalProjectGame.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinalProject
+{
+    public class FinalProjectGame : Game
+    {
+        private SpriteBatch m_spriteBatch;
+
+        public FinalProjectGame()
+        {
+            var graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 800,
+                PreferredBackBufferHeight = 600
+            };
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
