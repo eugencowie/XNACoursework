5cf920241fd6201b4e6a533dc9867593c9c46eaf
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-01-23 17:41:22 +0000

    Add button class

--- FinalProject/Source/Screens/Menu/MainMenuScreen.cs
+++ FinalProject/Source/Screens/Menu/MainMenuScreen.cs
@@ -6,6 +6,8 @@ namespace FinalProject
 {
     class MainMenuScreen : Screen
     {
+        private Button m_button;
+
         public MainMenuScreen(Game game, ScreenManager screenManager)
             : base(game, screenManager)
         {
@@ -13,6 +15,14 @@ namespace FinalProject
 
         public override void Initialize()
         {
+            m_button = new Button(
+                Content.Load<Texture2D>("Textures/Menu/Button_Cap"),
+                Content.Load<Texture2D>("Textures/Menu/Button_Center"),
+                Content.Load<SpriteFont>("Fonts/Menu"));
+
+            m_button.Position = Viewport.Bounds.Center.ToVector2();
+            m_button.Padding = new Vector2(25, 25);
+            m_button.Label = "Test button";
         }
 
         public override void Dispose()
@@ -25,10 +35,36 @@ namespace FinalProject
 
             if (kbs.IsKeyDown(Keys.Escape))
                 Game.Exit();
+
+            if (kbs.IsKeyDown(Keys.D1))
+                m_button.Label = "";
+
+            if (kbs.IsKeyDown(Keys.D2))
+            {
+                m_button.Label = "Test button";
+                m_button.Color = Color.Red;
+            }
+
+            if (kbs.IsKeyDown(Keys.D3))
+            {
+                m_button.Label = "Lorum ipsum delor sit amet";
+                m_button.Color = Color.Blue;
+            }
+
+            if (kbs.IsKeyDown(Keys.D4))
+            {
+                m_button.Label = "Lorum ipsum delor sit amet, the little brown dog jumped over the lazy cow";
+                m_button.Color = Color.Purple;
+            }
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            spriteBatch.Begin();
+
+            m_button.Draw(spriteBatch);
+
+            spriteBatch.End();
         }
     }
 }
---
+++ FinalProject/Source/Utilities/Button.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FinalProject
+{
+    class Button
+    {
+        public Texture2D CapTexture;
+        public Texture2D CenterTexture;
+        public Color Color = Color.Red;
+
+        public SpriteFont Font;
+        public Color LabelColor = Color.White;
+        public string Label;
+
+        public Vector2 Position;
+        public Vector2 Padding;
+
+        public bool Visible = true;
+
+        public Button(Texture2D capTexture, Texture2D centerTexture, SpriteFont font)
+        {
+            CapTexture = capTexture;
+            CenterTexture = centerTexture;
+            Font = font;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Visible && !string.IsNullOrWhiteSpace(Label))
+            {
+                Vector2 textSize = Font.MeasureString(Label);
+                Vector2 centerSize = textSize + Padding;
+
+                Rectangle leftCapRect = new Rectangle {
+                    Location = new Point(0, 0),
+                    Size = new Point(CapTexture.Width, (int)centerSize.Y)
+                };
+
+                Rectangle centerRect = new Rectangle {
+                    Location = leftCapRect.Location + new Point(leftCapRect.Width, 0),
+                    Size = new Point((int)centerSize.X, (int)centerSize.Y)
+                };
+
+                Rectangle rightCapRect = new Rectangle {
+                    Location = centerRect.Location + new Point(centerRect.Width, 0),
+                    Size = leftCapRect.Size
+                };
+
+                Vector2 textPos = centerRect.Location.ToVector2() + (Padding / 2f);
+
+                // Center the button on the position.
+                Vector2 totalSize = new Vector2(leftCapRect.Width + centerRect.Width + rightCapRect.Width, leftCapRect.Height);
+                Vector2 topLeft = Position - (totalSize / 2f);
+                leftCapRect.Offset(topLeft);
+                centerRect.Offset(topLeft);
+                rightCapRect.Offset(topLeft);
+                textPos += topLeft;
+
+                spriteBatch.Draw(CapTexture, leftCapRect, Color);
+                spriteBatch.Draw(CenterTexture, centerRect, Color);
+                spriteBatch.Draw(CapTexture, rightCapRect, null, Color, 0f, Vector2.Zero, SpriteEffects.FlipHorizontally, 0f);
+                spriteBatch.DrawString(Font, Label, textPos, LabelColor);
+            }
+        }
+    }
+}
