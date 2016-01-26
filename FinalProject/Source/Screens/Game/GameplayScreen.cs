58a79f98abcfaf1f046d35b54a58b52202af547d
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-01-26 19:55:42 +0000

    Add a very basic Pyxel Edit map loader

--- FinalProject/Source/Screens/Game/GameplayScreen.cs
+++ FinalProject/Source/Screens/Game/GameplayScreen.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace FinalProject
 {
@@ -10,6 +11,9 @@ namespace FinalProject
         private readonly Game m_game;
 
         private Player m_player;
+
+        private Map m_map;
+        private List<Texture2D> m_mapTextures = new List<Texture2D>();
 
         public GameplayScreen(Game game)
         {
@@ -22,6 +26,13 @@ namespace FinalProject
             {
                 Position = m_game.GraphicsDevice.Viewport.Bounds.Center.ToVector2()
             };
+
+            m_map = MapLoader.Load("Content/Maps/Test.txt");
+
+            for (int i=0; i<=23; i++)
+            {
+                m_mapTextures.Add(content.Load<Texture2D>("Maps/Test/tile" + i));
+            }
         }
 
         public void Dispose()
@@ -39,6 +50,23 @@ namespace FinalProject
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
+
+            foreach (var layer in m_map.Layers)
+            {
+                for (int y=0; y<layer.Tiles.Count; y++)
+                {
+                    for (int x=0; x<layer.Tiles[y].Count; x++)
+                    {
+                        Vector2 position = new Vector2(x * m_map.TileWidth, y * m_map.TileHeight);
+                        var tile = layer.Tiles[y][x];
+
+                        if (tile.Id != -1)
+                        {
+                            spriteBatch.Draw(m_mapTextures[tile.Id], position, Color.White);
+                        }
+                    }
+                }
+            }
 
             m_player.Draw(spriteBatch);
 
---
+++ FinalProject/Source/Utilities/Map.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FinalProject
+{
+    class Tile
+    {
+        public int Id;
+    }
+
+    class Layer
+    {
+        public int Id;
+        public List<List<Tile>> Tiles = new List<List<Tile>>();
+    }
+
+    class Map
+    {
+        public int TilesWide;
+        public int TilesHigh;
+
+        public int TileWidth;
+        public int TileHeight;
+
+        public List<Layer> Layers = new List<Layer>();
+    }
+
+    static class MapLoader
+    {
+        public static Map Load(string mapPath)
+        {
+            Map map = new Map();
+
+            if (!File.Exists(mapPath))
+            {
+                throw new System.ArgumentException("mapPath does not exist");
+            }
+
+            using (StreamReader file = new StreamReader(mapPath))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.StartsWith("tileswide"))
+                    {
+                        int.TryParse(line.Replace("tileswide ", ""), out map.TilesWide);
+                    }
+                    else if (line.StartsWith("tileshigh"))
+                    {
+                        int.TryParse(line.Replace("tileshigh ", ""), out map.TilesHigh);
+                    }
+                    else if (line.StartsWith("tilewidth"))
+                    {
+                        int.TryParse(line.Replace("tilewidth ", ""), out map.TileWidth);
+                    }
+                    else if (line.StartsWith("tileheight"))
+                    {
+                        int.TryParse(line.Replace("tileheight ", ""), out map.TileHeight);
+                    }
+                    else if (line.StartsWith("layer"))
+                    {
+                        Layer layer = new Layer();
+                        int.TryParse(line.Replace("layer ", ""), out layer.Id);
+
+                        while (!string.IsNullOrWhiteSpace(line = file.ReadLine()))
+                        {
+                            List<Tile> row = new List<Tile>();
+
+                            foreach (string tileString in line.Split(','))
+                            {
+                                if (!string.IsNullOrWhiteSpace(tileString))
+                                {
+                                    Tile tile = new Tile();
+                                    int.TryParse(tileString, out tile.Id);
+                                    
+                                    row.Add(tile);
+                                }
+                            }
+
+                            layer.Tiles.Add(row);
+                        }
+
+                        map.Layers.Add(layer);
+                    }
+                }
+            }
+
+            return map;
+        }
+    }
+}
